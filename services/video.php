131cf4977c3dbdd544e78ed80e475eb2f8192d66
ilyaburlakov <burlakov@groupintegra.ru>
2026-09-20 23:39:04 +0300

    fix: исправить PHP-структуру SEO-данных

--- services/video.php
+++ services/video.php
@@ -1,6 +1,6 @@
 <?php
 define('INTEGRA_SERVICE_PAGE', true);
-$service = $service = json_decode('{
+$service = json_decode('{
     "title": "Видеонаблюдение — проектирование и монтаж CCTV | Группа Интегра",
     "name": "Системы видеонаблюдения",
     "serviceType": "Проектирование и монтаж видеонаблюдения",
