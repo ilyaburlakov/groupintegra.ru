8cf69b3d616d92fa870ab87aa87f2ddf3d8c9097
ilyaburlakov <burlakov@groupintegra.ru>
2026-09-20 20:56:53 +0300

    seo: fix weak-current canonical and OG URLs

--- slabotchka.php
+++ slabotchka.php
@@ -8,10 +8,10 @@
     <meta property="og:title" content="Слаботочные системы - Комплексные решения | Группа Интегра">
     <meta property="og:description" content="Профессиональное проектирование и монтаж слаботочных систем любой сложности с гарантией 5 лет">
     <meta property="og:type" content="website">
-    <meta property="og:url" content="https://groupintegra.ru/weak-current.php">
+    <meta property="og:url" content="https://groupintegra.ru/slabotchka.php">
     <meta property="og:image" content="/images/weak-current-hero.jpg">
     <meta name="robots" content="index, follow">
-    <link rel="canonical" href="https://groupintegra.ru/weak-current.php">
+    <link rel="canonical" href="https://groupintegra.ru/slabotchka.php">
     <link rel="icon" href="/images/favicon.ico" type="image/x-icon">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
